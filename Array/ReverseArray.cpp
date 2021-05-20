d8f02569b1dc98d82803ddc1888f7700fd1e17de
voiduday <udaypratapyadaw03@gmail.com>
2021-05-20 22:20:04 +0530

    done

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void reverseArray(int arr[], int start, int end){
+        while (start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        
+}
+void printArray(int arr[], int n){
+    for (int  i = 0; i < n; i++)
+    {
+        cout << arr[i]<< " ";
+    }
+    cout << endl;
+}
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+  
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    printArray(arr, n);
+    reverseArray(arr , 0 , n-1);
+    printArray(arr, n);
+    return 0;
+}
---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+void printArray(int arr[], int n){
